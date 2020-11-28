c53fdd9d9a55736c882c8d373bf59dfa25c3fb4d
Alexandra Kubatieva <alexandra.kubatieva@gmail.com>
2020-11-28 22:19:36 +0000

    updated sack class

--- game_parts.py
+++ game_parts.py
@@ -8,50 +8,65 @@ class Player:
         self.player_card = player_card
 
     def cross(self, current_ball_from_sack):
-        updated_player_card = ['-' if number == current_ball_from_sack else number for number in self.player_card]
-        self.player_card = updated_player_card
-        return self.player_card
-
+        pass
     # def pass_round(self, )
 
 
 class Sack:
-    def __init__(self, current_ball_number):
+    def __init__(self):
         self.balls = list(range(1, 91))
-        self.current_ball_number = current_ball_number
 
     def get_current_number(self):
-        self.current_ball_number = random.randint(1, 90)
-        self.balls.remove(self.current_ball_number)
-        return self.current_ball_number
+        current_ball_index = random.choice(range(len(self.balls)))
+        current_ball_number = self.balls[current_ball_index]
+        del self.balls[current_ball_index]
+        return current_ball_number
 
 
 class Card:
     def __init__(self):
-        self.card_numbers = random.sample(range(1, 91), 15)
-
-    def print_card(self):
-        chunked_card_numbers = list(self.__chunks(self.card_numbers, 5))
-        card_rows = []
-        for chunk in chunked_card_numbers:
-            chunk.sort()
-            chunk = [str(n) for n in chunk]
-            row = ' '.join(chunk)
-            card_rows.append(row)
-        card_str = '\n'.join(card_rows)
-        print(card_str)
+        card_numbers = random.sample(range(1, 91), 15)
+        self.card_rows = list(self.__chunks(card_numbers, 5))
+        for row in self.card_rows:
+            row.sort()
 
     def __chunks(self, numbers, n):
         for i in range(0, len(numbers), n):
             yield numbers[i:i + n]
 
-    def check_current_number(self, current_number):
-        if current_number in self.card_numbers:
-            pass
-        else:
-            print(f'The number {current_number} is not in your card. You lose')
+    def print_card(self):
+        _ = []
+        for row in self.card_rows:
+            row = [str(n).rjust(3) for n in row]
+            row = ' '.join(row)
+            _.append(row)
+        card_str = '\n'.join(_)
+        print(card_str)
+
+    def cross_number(self, number_to_cross):
+        updated_rows = []
+        for row in self.card_rows:
+            updated_row = ['-' if number == number_to_cross else number for number in row]
+            updated_rows.append(updated_row)
+        self.card_rows = updated_rows
+        return self.card_rows
+
+    def contains(self, number):
+        for row in self.card_rows:
+            if number in row:
+                return True
+        return False
 
 
-c = Card()
-c.print_card()
-c.print_card()
+s = Sack()
+for i in range(91):
+    print(s.get_current_number())
+
+# c = Card()
+# c.print_card()
+# c.cross_number(c.card_rows[0][1])
+# c.cross_number(c.card_rows[1][3])
+# c.cross_number(c.card_rows[2][2])
+# print(c.contains(123))
+# print(c.contains(c.card_rows[0][3]))
+# c.print_card()
